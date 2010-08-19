320d81bfca0e8624561dcae6fee18f26660042d0
Cicero Oliveira <cicero@cicerooliveira.com>
2010-08-18 22:17:39 -0400

    Added home page links test

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,9 @@ module ApplicationHelper
       "#{base_title} | #{@title}"
     end
   end
+  
+  #Return logo
+  def logo
+    image_tag("logo.png", :alt => "Peep Community", :class => "round")
+  end
 end
